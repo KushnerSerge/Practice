54742472f1bd56ddc49e9e31b72679917c283ad9
SERGEKUSHNER <kusnir.sergiu@gmail.com>
2022-03-11 13:31:45 +0200

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public string Name { get; set; }    
+        public string Description { get; set; }
+
+        public Class1(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public void SomeMethod()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
